67d8cbc388fa99d3b29de50b89cbf5b44d68c766
RP082024 <161113339+RP082024@users.noreply.github.com>
2024-06-06 11:23:55 +0200

    Title & 1. section with some content
    added i glyph, and ribbon

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     setTimeout(function () {
         const image = document.getElementById('unfoldImage');
         image.style.transform = 'scaleX(1)';
-    }, 3000); // 3000 milliseconds = 3 seconds
+    }, 2000); // 2000 milliseconds = 2 seconds
 
     // Toggle hamburger menu
     const hamburgerMenu = document.querySelector('.hamburger-menu');
@@ -11,3 +11,19 @@ window.onload = function () {
         menuItems.classList.toggle('show');
     });
 };
+
+
+function togglePopup() {
+    var popup = document.querySelector('.popup');
+    popup.style.display = popup.style.display === 'block' ? 'none' : 'block';
+}
+
+// Close the popup if clicked outside
+window.onclick = function (event) {
+    var popups = document.querySelectorAll('.popup');
+    popups.forEach(function (popup) {
+        if (!popup.contains(event.target) && !event.target.classList.contains('info-glyph')) {
+            popup.style.display = 'none';
+        }
+    });
+}
